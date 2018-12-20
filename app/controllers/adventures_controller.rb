4ff183ca67c6be739456d123657b0dcf71eb06e3
Miranda Short <mird2794@gmail.com>
2018-12-20 09:02:11 -0500

    states

---
+++ app/controllers/adventures_controller.rb
@@ -0,0 +1,7 @@
+class AdventuresController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :states
+
+  get 'index' => 'adventures#index'
+  get 'about' => 'adventures#about'
+  get 'ilovetocode' => 'adventures#index'
+  root 'adventures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
