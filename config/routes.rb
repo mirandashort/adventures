f80937af9ff3dcbcc9cbfd2aa9c2614048981ba3
Miranda Short <mird2794@gmail.com>
2018-12-20 09:49:08 -0500

    activities

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :states
+  resources :states, :activities
 
   get 'index' => 'adventures#index'
   get 'about' => 'adventures#about'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_013125) do
+ActiveRecord::Schema.define(version: 2018_12_21_145406) do
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.integer "cost"
+    t.integer "duration"
+    t.string "duration_interval"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "payment_interval"
+  end
 
   create_table "states", force: :cascade do |t|
     t.string "name"
